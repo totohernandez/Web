55fc3f4f521a9b0dcf12635155a93b0a1d297e5a
Ricardo Hernandez <toto@Ricardos-MacBook-Air.local>
2021-09-03 23:34:36 -0600

    js

---
+++ Javacript/1-intro.js
@@ -0,0 +1,18 @@
+//Javascript can change HTML Content
+
+//method getElementById() finds an HTML element with id "demo"
+//and changes content innHTML to "Hello Javascript"
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+
+//JavaScript can change HTML Styles CSS
+document.getElementById("demo").style.fontSize = "35px";
+
+//JavaScript can hide HTML elements
+document.getElementById("demo").style.display= "none";
+
+//JavaScript can show HTML elements
+document.getElementById("demo").style.display = "block";
+
+
+
